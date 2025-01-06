f4495cf34fd40df52e821768e0110c3738245d77
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2025-01-06 21:35:26 +0530

    6Jan

---
+++ src/templates/SideNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const SideNav = () => {
+    return (
+        <>
+            <div className='w-[20%] h-full border-r-2 border-zinc-400 p-10'>
+                <h1 className='text-2xl font-bold mr-2 text-white'>
+                    <i className="text-[#6556CD] ri-tv-fill "></i>
+                    <span> movie-app</span></h1>
+                <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
+                    <h1 className='text-white font-semibold text-xl mt-10 mb-5'>New Feeds</h1>
+                    <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5'><i className="mr-2 ri-fire-fill"></i> Trending</Link>
+                    <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5'><i className="mr-2 ri-shining-fill"></i> Popular</Link>
+                    <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5'><i className="mr-2 ri-movie-2-fill"></i> Movies</Link>
+                    <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5'> <i className="mr-2 ri-tv-fill"></i>TV shows</Link>
+                    <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5'><i className=" mr-2 ri-team-fill"></i> People</Link>
+                   <hr className='border-noneh-[1px] bg-zinc-100' />
+                </nav>
+                <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
+                    <h1 className='text-white font-semibold text-xl mt-10 mb-5'>website Information</h1>
+                    <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5'><i className="mr-2 ri-information-fill"></i>About</Link>
+                    <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5'><i className=" mr-2 ri-phone-fill"></i>Contact</Link>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default SideNav
---
+++ src/templates/TopNav.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+const TopNav = () => {
+    const [query,setquery]=useState("");
+    console.log(query)
+    return (
+        <div className='w-full h-[10vh] flex justify-center items-center'>
+            <i className=" text-3xl text-zinc-400 ri-search-line"></i>
+            <input onChange={(e)=> setquery(e.target.value)}
+            value={query}
+             className='w-[50%] mx-10 p-5 text-xl outline-none bg-transparent text-zinc-200' type="text" placeholder='Serach anything' />
+            <i className=" text-3xl text-zinc-400 ri-close-fill"></i>
+            <div className='absolute w-[50%] h-[50vh] bg-zinc-200 top-[20%] overflow-auto'>
+                <Link className='text-zinc-600 font-semibold hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>heyyty evryone</span>
+                </Link>
+                <Link className='text-zinc-600 font-semibold hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>heyyty evryone</span>
+                </Link><Link className='text-zinc-600 font-semibold hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>heyyty evryone</span>
+                </Link><Link className='text-zinc-600 font-semibold hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>heyyty evryone</span>
+                </Link><Link className='text-zinc-600 font-semibold hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>heyyty evryone</span>
+                </Link><Link className='text-zinc-600 font-semibold hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>heyyty evryone</span>
+                </Link>
+            </div>
+        </div>
+
+    )
+}
+
+export default TopNav
